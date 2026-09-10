6e5cad1e791ccb441750d329452a9e8c8e860cf8
danielbonkowsky <danielbonkowsky@gmail.com>
2026-09-10 15:42:08 -0700

    monadify

--- theories/Compiler.v
+++ theories/Compiler.v
@@ -6,6 +6,8 @@ From compcert Require Import export.Ctypesdefs.
 Import ListNotations.
 Import Errors.
 
+Local Open Scope error_monad_scope.
+
 (* tag type *)
 Definition ident_of_func     (i : N) : AST.ident := ((N.succ_pos i)~0~0~0)%positive.
 Definition ident_of_global   (i : N) : AST.ident := ((N.succ_pos i)~0~0~1)%positive.
@@ -31,12 +33,9 @@ Fixpoint wasm_types_to_clight_types (ts : list value_type)
   : res (list Ctypes.type) :=
   match ts with
   | nil => OK nil
-  | t :: rest =>
-    match wasm_type_to_clight_type t, wasm_types_to_clight_types rest with
-    | OK t', OK rest' => OK (t' :: rest')
-    | Error err, _ => Error err
-    | _, Error err => Error err
-    end
+  | t :: rest => do t' <- wasm_type_to_clight_type t;
+                 do rest' <- wasm_types_to_clight_types rest;
+                 OK (t' :: rest')
   end.
 
 (** turn a list of Wasm variables into a list of Clight variables. base is the 
@@ -45,13 +44,9 @@ Fixpoint wasm_vars_to_clight_vars (base : N) (ts : list value_type)
   : res (list (AST.ident * Ctypes.type)) :=
   match ts with
   | nil => OK nil
-  | t :: ts' =>
-    match wasm_type_to_clight_type t,
-          wasm_vars_to_clight_vars (N.succ base) ts' with
-    | OK ty, OK rest =>  OK (((ident_of_local base), ty) :: rest)
-    | Error err, _ => Error err
-    | _, Error err => Error err
-    end
+  | t :: ts' => do ty <- wasm_type_to_clight_type t;
+                do rest <- wasm_vars_to_clight_vars (N.succ base) ts';
+                OK (((ident_of_local base), ty) :: rest)
   end.
 
 (** parameters start at identifier 0 *)
@@ -68,21 +63,13 @@ Definition wasm_return_to_clight_return (ts : list value_type)
   | _ :: _ :: _ => Error (msg "multi-value return not supported")
   end.
 
-(** split Wasm function_type into Clight return type, calling convention, and 
-    parameters *)
+(** split Wasm function_type into Clight return type, and parameters *)
 Definition clight_of_functype (tf : function_type)
-  : res (
-    Ctypes.type * 
-    AST.calling_convention * 
-    list (AST.ident * Ctypes.type)
-  ) :=
+  : res (Ctypes.type * list (AST.ident * Ctypes.type)) :=
   let 'Tf ts1 ts2 := tf in
-    match wasm_return_to_clight_return ts2, 
-          wasm_params_to_clight_params ts1 with
-    | OK ret, OK ps => OK (ret, AST.cc_default, ps)
-    | Error err, _ => Error err
-    | _, Error err => Error err
-    end
+    do ret <- wasm_return_to_clight_return ts2;
+    do ps <- wasm_params_to_clight_params ts1;
+    OK (ret, ps)
   .
 
 (** compiler state records the current stack and the max depth of the stack *)
@@ -113,19 +100,15 @@ Definition slot_ident (t : value_type) (d : N) : res AST.ident :=
 (** return a Clight expression for the variable at depth d in the stack. depth 0
     is the head, etc. *)
 Definition slot_expr (t : value_type) (d : N) : res Clight.expr :=
-  match slot_ident t d, wasm_type_to_clight_type t with
-  | OK id, OK ty => OK (Clight.Etempvar id ty)
-  | Error err, _ => Error err
-  | _, Error err => Error err
-  end.
+  do id <- slot_ident t d;
+  do ty <- wasm_type_to_clight_type t;
+  OK (Clight.Etempvar id ty).
 
 (** return a Clight statement representing a push to the stack *)
 Definition push_expr (t : value_type) (cs : compiler_state) (e : Clight.expr)
   : res (list Clight.statement * compiler_state) :=
-  match slot_ident t (depth cs.(stack)) with
-  | OK id => OK ([Clight.Sset id e], cs_push cs t)
-  | Error err => Error err
-  end.
+  do id <- slot_ident t (depth cs.(stack));
+  OK ([Clight.Sset id e], cs_push cs t).
 
 (** convert a single Wasm basic_instruction to 1+ Clight statements *)
 Definition instr_to_statement (cs : compiler_state) (instr : basic_instruction) 
@@ -142,16 +125,13 @@ Definition instr_to_statement (cs : compiler_state) (instr : basic_instruction)
     match op', cs.(stack) with 
     | BOI_add, T_num T_i32 :: T_num T_i32 :: rest =>
       let d2 := depth rest in
-        match slot_ident (T_num T_i32) d2,
-              slot_expr  (T_num T_i32) d2,
-              slot_expr  (T_num T_i32) (N.succ d2) with
-        | OK id2, OK e1, OK e2 =>
-          OK ([Clight.Sset id2 (Clight.Ebinop Cop.Oadd e1 e2 tuint)],
-                cs_push {| stack := rest; max_depth := cs.(max_depth) |} (T_num T_i32))
-        | Error err, _, _ => Error err
-        | _, Error err, _ => Error err
-        | _, _, Error err=> Error err
-        end
+        do id2 <- slot_ident (T_num T_i32) d2;
+        do e1  <- slot_expr  (T_num T_i32) d2;
+        do e2  <- slot_expr  (T_num T_i32) (N.succ d2);
+        OK (
+          [Clight.Sset id2 (Clight.Ebinop Cop.Oadd e1 e2 tuint)],
+          cs_push {| stack := rest; max_depth := cs.(max_depth) |} (T_num T_i32)
+        )
     | _, _ => Error (msg "unsupported i32 binary operation")
     end
   | _ => Error (msg "unsupported instruction")
@@ -169,14 +149,9 @@ Fixpoint instrs_to_statements
   match body with
   | nil => OK (nil, cs)
   | instr :: body' =>
-    match instr_to_statement cs instr with
-    | OK (ss, cs') =>
-      match instrs_to_statements cs' body' with
-      | OK (ss', cs'') => OK (ss ++ ss', cs'')
-      | Error err => Error err
-      end
-    | Error err => Error err
-    end
+    do (ss, cs') <- instr_to_statement cs instr;
+    do (ss', cs'') <- instrs_to_statements cs' body';
+    OK (ss ++ ss', cs'')
   end.
 
 (** default compiler state has empty stack *)
@@ -191,10 +166,8 @@ Definition return_stmt (ret_type : list value_type) (cs : compiler_state)
   | nil, nil => OK (Clight.Sreturn None)
   | t :: nil, t' :: rest =>
     if value_type_eqb t t' then
-      match slot_expr t (depth rest) with
-      | OK e => OK (Clight.Sreturn (Some e))
-      | Error err => Error err
-      end
+      do e <- slot_expr t (depth rest);
+      OK (Clight.Sreturn (Some e))
     else Error (msg "stack and return type mismatch")
   | nil, _ :: _ => Error (msg "function returns nothing but values are left on the operand stack")
   | _ :: nil, nil => Error (msg "function must return a value but the operand stack is empty")
@@ -204,14 +177,9 @@ Definition return_stmt (ret_type : list value_type) (cs : compiler_state)
 (** compile the body of a Wasm function into a Clight statement *)
 Definition compile_body (ret_type : list value_type) (body : expr) 
   : res (Clight.statement * compiler_state) :=
-  match instrs_to_statements cs_initial body with
-  | OK (ss, cs) => 
-    match return_stmt ret_type cs with
-    | OK ret => OK (seq_of_list (ss ++ [ret]), cs)
-    | Error err => Error err
-    end
-  | Error err => Error err
-  end.
+  do (ss, cs) <- instrs_to_statements cs_initial body;
+  do ret <- return_stmt ret_type cs;
+  OK (seq_of_list (ss ++ [ret]), cs).
 
 (** generate a list of h Clight temps of a given type *)
 Definition slot_temps (mk : N -> AST.ident) (ty : Ctypes.type) (h : N)
@@ -224,31 +192,20 @@ Definition slot_temps (mk : N -> AST.ident) (ty : Ctypes.type) (h : N)
 Definition compile_func (m : module) (func : module_func) 
   : res Clight.function :=
   match lookup_N m.(mod_types) func.(modfunc_type) with
-  | Some (Tf ts1 ts2 as func_type) =>
-    match clight_of_functype func_type with
-    | OK (ret_type, cc, params) =>
-      (* local idxs start after params *)
-      match wasm_vars_to_clight_vars (N.of_nat (List.length ts1))
-                                         func.(modfunc_locals) with
-      | OK locals =>
-        match compile_body ts2 func.(modfunc_body) with
-        | OK (body, cs) =>
-          let stack_temps := (
-            slot_temps ident_of_i32_slot tuint cs.(max_depth) ++
-            slot_temps ident_of_i64_slot tulong cs.(max_depth) ++
-            slot_temps ident_of_f32_slot tfloat cs.(max_depth) ++
-            slot_temps ident_of_f64_slot tdouble cs.(max_depth) ++
-            slot_temps ident_of_ref_slot (tptr tvoid) cs.(max_depth)
-          ) in
-            OK (
-              Clight.mkfunction ret_type cc params nil (params ++ locals ++ stack_temps) body
-            )
-        | Error err => Error err
-        end
-      | Error err => Error err
-      end
-    | Error err => Error err
-    end
+  | Some (Tf ts1 ts2 as func_type) => 
+    do (ret_type, params) <- clight_of_functype func_type;
+    do locals <- wasm_vars_to_clight_vars (N.of_nat (List.length ts1)) func.(modfunc_locals);
+    do (body, cs) <- compile_body ts2 func.(modfunc_body);
+    let stack_temps := (
+      slot_temps ident_of_i32_slot tuint cs.(max_depth) ++
+      slot_temps ident_of_i64_slot tulong cs.(max_depth) ++
+      slot_temps ident_of_f32_slot tfloat cs.(max_depth) ++
+      slot_temps ident_of_f64_slot tdouble cs.(max_depth) ++
+      slot_temps ident_of_ref_slot (tptr tvoid) cs.(max_depth)
+    ) in
+      OK (
+        Clight.mkfunction ret_type AST.cc_default params nil (params ++ locals ++ stack_temps) body
+      )
   | None => Error (msg "function type couldn't be found in binary")
 end.
 
@@ -267,22 +224,17 @@ Fixpoint compile_func_imports (m : module) (idx : N) (imps : list module_import)
     | MID_func tidx =>
       match lookup_N m.(mod_types) tidx with
       | Some (Tf ts1 ts2) =>
-        (* external function types take list of types, not list of (ident * type) *)
-        match wasm_types_to_clight_types ts1,
-              wasm_return_to_clight_return ts2,
-              compile_func_imports m (N.succ idx) rest with
-        | OK args, OK ret, OK rest' =>
-          let sg := Ctypes.signature_of_type args ret AST.cc_default in
+        do args <- wasm_types_to_clight_types ts1;
+        do ret <- wasm_return_to_clight_return ts2;
+        do rest' <- compile_func_imports m (N.succ idx) rest;
+        let sg := Ctypes.signature_of_type args ret AST.cc_default in
           OK (
-            (ident_of_func idx,
-            AST.Gfun (Ctypes.External
-                      (AST.EF_external (string_of_name imp.(imp_name)) sg) 
-                      args ret AST.cc_default)) :: rest'
+              (ident_of_func idx,
+               AST.Gfun (Ctypes.External
+                        (AST.EF_external (string_of_name imp.(imp_name)) sg) 
+                        args ret AST.cc_default)
+              ) :: rest'
           )
-        | Error err, _, _ => Error err
-        | _, Error err, _ => Error err
-        | _, _, Error err => Error err
-        end
       | None => Error (msg "imported function type can't be found")
       end
     | _ => compile_func_imports m idx rest (* if it's not a function just skip *)
@@ -296,12 +248,9 @@ Fixpoint compile_funcs_from (m : module) (idx : N) (funcs : list module_func)
   match funcs with
   | nil => OK nil
   | f :: rest =>
-    match compile_func m f, compile_funcs_from m (N.succ idx) rest with
-    | OK cf, OK rest' =>
-      OK ((ident_of_func idx, AST.Gfun (Ctypes.Internal cf)) :: rest')
-    | Error err, _ => Error err
-    | _, Error err => Error err
-    end
+    do cf <- compile_func m f;
+    do rest' <- compile_funcs_from m (N.succ idx) rest;
+    OK ((ident_of_func idx, AST.Gfun (Ctypes.Internal cf)) :: rest')
   end.
 
 (** count the number of imported functions in a module *)
@@ -320,9 +269,6 @@ Definition compile_funcs (m : module)
 (** Note: module defined in WasmCert-Coq/theories/datatypes.v:740; 
     Clight.program defined in CompCert/cfrontend/Ctypes.v:1545 *)
 Definition compile (m : module) : Errors.res Clight.program :=
-  match compile_func_imports m 0 m.(mod_imports), compile_funcs m with
-  | OK imports, OK defs =>
-    Ctypes.make_program nil (imports ++ defs) nil 1%positive
-  | Error err, _ => Error err
-  | _, Error err => Error err
-  end.
+  do imports <- compile_func_imports m 0 m.(mod_imports);
+  do defs <- compile_funcs m;
+  Ctypes.make_program nil (imports ++ defs) nil 1%positive.
