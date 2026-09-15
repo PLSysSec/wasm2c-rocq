42a80bf7db8f7fb8be09579291a788b2541d6486
danielbonkowsky <danielbonkowsky@gmail.com>
2026-09-15 11:40:56 -0700

    thread inst pointer thru compile.v + fixes

--- theories/Ident.v
+++ theories/Ident.v
@@ -10,7 +10,6 @@ Definition ident_of_i64_slot   (i : N) : AST.ident := ((N.succ_pos i)~0~1~0~0)%p
 Definition ident_of_f32_slot   (i : N) : AST.ident := ((N.succ_pos i)~0~1~0~1)%positive.
 Definition ident_of_f64_slot   (i : N) : AST.ident := ((N.succ_pos i)~0~1~1~0)%positive.
 Definition ident_of_ref_slot   (i : N) : AST.ident := ((N.succ_pos i)~0~1~1~1)%positive.
-Definition ident_of_inst       (i : N) : AST.ident := ((N.succ_pos i)~1~0~0~0)%positive. (* think this is kinda unnecessary since only 1 mem allowed*)
 Definition ident_of_inst_field (i : N) : AST.ident := ((N.succ_pos i)~1~0~0~1)%positive.
 Definition ident_of_struct     (i : N) : AST.ident := ((N.succ_pos i)~1~0~1~0)%positive.
 Definition ident_of_runtime    (i : N) : AST.ident := ((N.succ_pos i)~1~0~1~1)%positive.
@@ -18,3 +17,4 @@ Definition ident_of_data       (i : N) : AST.ident := ((N.succ_pos i)~1~1~0~0)%p
 
 Definition ident_calloc      : AST.ident := ident_of_runtime 0.
 Definition ident_instantiate : AST.ident := ident_of_runtime 1.
+Definition ident_inst     : AST.ident := (1~1~0~0~0)%positive.
